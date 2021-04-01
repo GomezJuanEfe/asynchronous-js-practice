4d0246a7b295c324235a415ced5a508029944c2d
Juan Gomez <jf.gomezramiez@gmail.com>
2021-04-01 16:22:17 -0500

    Avance hasta la clase Peticion a APIs usando Callbacks

---
+++ src/callback/challenge.js
@@ -0,0 +1,35 @@
+let XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+
+/*
+1. Vamos a crear la función (fetchData) que me va a permitir traer la información de la API, que también va a hacer un callback y va a desencadenar los llamados que necesitamos.
+
+2. Dentro de la función vamos a empezar a construir la petición por medio de XMLHttpRequest. Para eso vamos a instanciar este elemento.
+
+3. Luego voy a codificar la apertura por medio del método open.
+
+4. Vamos a generar o excuchar lo que va a generar esa apertura con xhttp.onreadystatechange... Si esta cambio suscede entonces se va a ejecutar una función.
+
+5. Una vez en esta función voy a tener un espacio para hacer una validación para poder ejecutar mi callback. Básicamente es si el estado en el cual se encuentra es satisfactorio.
+
+6. Una vez se cumpla esta validación voy a tener que "parsear" el resultado de mi petición para poder tratarlo mejor.
+
+7. Por último, con el método send, mando mi solicitud.
+
+8. De esta forma tengo la función que le va a dar vida a mis peticiones.
+*/
+
+function fetchData(url_api, callback) {
+    let xhttp = new XMLHttpRequest();
+    xhttp.open('GET', url_api, true);
+    xhttp.onreadystatechange = function (event) {
+        if (xhttp.readyState === 4) {
+            if (xhttp.status === 200){
+                callback(null, JSON.parse(xhttp.responseText));
+            } else {
+                const error = new Error('Error ' + url_api);
+                return callback(error, null)
+            }
+        }
+    }
+    xhttp.send();
+}
